7727c093a5e192d92982e542f6867ae89175b10e
Donatas Dicmanas <donatas.dicmanas@gmail.com>
2023-04-12 20:20:11 +0300

    type correction

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -5,19 +5,19 @@ echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
 set -x
-grandle jar:jar install:install help:evaluate -Dexpression=project.name
+gradle jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
 
 echo 'The following complex command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME=`grandle help:evaluate -Dexpression=project.name | grep "^[^\[]"`
+NAME=`gradle help:evaluate -Dexpression=project.name | grep "^[^\[]"`
 set +x
 
 echo 'The following complex command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION=`grandle help:evaluate -Dexpression=project.version | grep "^[^\[]"`
+VERSION=`gradle help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
